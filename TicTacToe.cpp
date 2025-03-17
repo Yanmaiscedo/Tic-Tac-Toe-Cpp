38425e33cfd5d388504444363fc954256e4056af
Yan <134351027+Yanmaiscedo@users.noreply.github.com>
2025-03-16 22:43:14 -0300

    Feat: Draw function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -2,6 +2,26 @@
 
 using namespace std;
 
+char matrix[3][3] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+char player = 'X';
+int n;
+
+void Draw()
+{
+    system("cls");
+    cout << "!!!!El Roego De la Abuelita!!!!" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << "      " << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+
+
 int main()
 {
 
